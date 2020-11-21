4f48c9806b27bba8dc026dcb8f009b808995b61e
xwoud <xwoud@naver.com>
2020-11-21 20:23:34 +0900

    Apply .gitignore

---
+++ nalul_iOS/nalul_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nalul_iOS
+//
+//  Created by 김민희 on 2020/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
