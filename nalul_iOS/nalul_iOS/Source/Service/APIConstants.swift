d2322b9d07c90555589d2d403f3796491db845be
xwoud <xwoud@naver.com>
2020-11-21 23:14:54 +0900

    [Add] server url 파일 추가

---
+++ nalul_iOS/nalul_iOS/Source/Service/APIConstants.swift
@@ -0,0 +1,25 @@
+//
+//  APIConstants.swift
+//  nalul_iOS
+//
+//  Created by 김민희 on 2020/11/21.
+//
+
+import Foundation
+struct APIConstants {
+    static let baseURL = "http://"
+    
+    //uuid 체크인 url
+    static let loginURL = APIConstants.baseURL + ""
+    
+    // 메인 사진 9개 받아오는 url
+    static let mainURL = APIConstants.baseURL + ""
+    
+    // 디테일 사진 받아오는 url
+    static let detailURL = APIConstants.baseURL + ""
+    
+    // 글 작성 url
+    static let writeURL = APIConstants.baseURL + ""
+    
+    
+}
