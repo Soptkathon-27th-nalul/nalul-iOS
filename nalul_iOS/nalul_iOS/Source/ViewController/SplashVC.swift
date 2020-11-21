37e53c4bd4d8136ac8b9f95bd6bfdfae3c9ae054
xwoud <xwoud@naver.com>
2020-11-22 05:34:57 +0900

    [Update] Splash 이미지 추가

--- nalul_iOS/nalul_iOS/Source/ViewController/SplashVC.swift
+++ nalul_iOS/nalul_iOS/Source/ViewController/SplashVC.swift
@@ -16,8 +16,8 @@ class SplashVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     @objc func aaaa() {
-//           guard let storyboard = UIStoryboard(name: "OnBoarding", bundle: nil).instantiateViewController(identifier: "SignInVC") as? SignInVC else { return }
-//           UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController = storyboard
+           guard let storyboard = UIStoryboard(name: "Write", bundle: nil).instantiateViewController(identifier: "WriteVC") as? WriteVC else { return }
+           UIApplication.shared.windows.filter {$0.isKeyWindow}.first?.rootViewController = storyboard
        }
     private func CheckInUUID(uuid: String) {
         LoginService.shared.login(uuid: uuid) { networkResult in switch
@@ -26,6 +26,12 @@ class SplashVC: UIViewController {
         case .success(let res):
             let response = res as! LoginData<LoginSuccess>
             self.UuidModel = response
+            UserDefaults.standard.set(response.data?.accessToken,forKey: "accessToken")
+            if response.status == 200 {
+                
+            } else if response.status == 204 {
+                
+            }
         case .requestErr(_):
             print("requestErr")
         case .pathErr:
